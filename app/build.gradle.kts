59ff3b5d4cb9b250ca773839469d01f5b706466d
Felipe Zea <48108543+Felipe27200@users.noreply.github.com>
2026-04-16 19:41:34 -0500

    agregar icono

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,4 +74,11 @@ dependencies {
     implementation("androidx.compose.runtime:runtime-livedata:1.7.8")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("com.google.android.material:material:1.12.0")
+
+    // --- LIBRERÍAS DE PRUEBAS (Faltantes) ---
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.12.01"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 }
